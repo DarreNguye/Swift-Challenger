f684263ae67b6a70426afd473f50f4cdf24e75ee
DarreNguye <darrenbanguyen@gmail.com>
2024-02-24 15:17:55 -0600

    Initial

---
+++ Swift Challenger/GameState.swift
@@ -0,0 +1,8 @@
+//
+//  GameState.swift
+//  Swift Challenger
+//
+//  Created by 64005831 on 2/23/24.
+//
+
+import Foundation
---
+++ Swift Challenger/VehicleSelect.swift
@@ -0,0 +1,18 @@
+//
+//  VehicleSelect.swift
+//  Swift Challenger
+//
+//  Created by 64005831 on 2/23/24.
+//
+
+import SwiftUI
+
+struct VehicleSelect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VehicleSelect()
+}
